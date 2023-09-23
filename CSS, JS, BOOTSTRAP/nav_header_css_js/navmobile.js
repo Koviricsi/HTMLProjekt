387ae3e83809c62098887a5b5ce0f18e3adb1e1d
Koviricsi <richard.kovi.07@gmail.com>
2023-09-23 11:35:26 +0200

    g

--- CSS, JS, BOOTSTRAP/nav_header_css_js/navmobile.js
+++ CSS, JS, BOOTSTRAP/nav_header_css_js/navmobile.js
@@ -1,6 +1,8 @@
 function navMenu(){
   const navLinks = document.querySelectorAll(".navlink");
+  const navLogo = document.getElementById("navlogo");
+  let rotation = 1;
   navLinks.forEach((i) => {
     i.classList.toggle("active");
-  })
+  });
 }
